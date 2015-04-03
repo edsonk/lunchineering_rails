08c2ef000c4bac5545763139b1565ab3b518063c
Kyle Edson <kae@foraker.com>
2015-04-03 12:36:02 -0600

    Add destinations + serializer/endpoints

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,34 @@
+class DestinationsController < ApplicationController
+  def index
+    render json: Destination.all
+  end
+
+  def show
+    render json: destination
+  end
+
+  def update
+    destination.update_attributes(destination_params)
+    render json: destination
+  end
+
+  def create
+    destination = Destination.create(destination_params)
+    render json: destination
+  end
+
+  def destroy
+    destination.destroy
+    head :no_content
+  end
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:name)
+  end
+
+  def destination
+    @destination ||= Destination.find(params[:id])
+  end
+end
